173d30854f6729fdf4581f61b44326a21b886929
Hieu <nguyendinhminhhieu0409@gmail.com>
2020-04-16 12:02:10 +0700

    add Product detail view

--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 # Create your views here.
@@ -8,10 +8,10 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = 'products/list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
 
 def product_list_view(request):
     queryset = Product.objects.all()
@@ -19,3 +19,20 @@ def product_list_view(request):
         'object_list':queryset
     }
     return render(request, 'products/list.html', context)
+
+
+class ProductDetailView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'products/detail.html'
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+def product_detail_view(request, pk, *args,**kwargs):
+    instance = get_object_or_404(Product, pk=pk) #id
+    context = {
+        'object':instance
+    }
+    return render(request, 'products/detail.html', context)
